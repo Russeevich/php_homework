7a2cb07a088baf967a24cf49eac4b46bc6d4ecd0
Russeevich <kokanazar@yandex.ru>
2021-08-18 13:07:30 +0300

    update

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?
-    const ALL_PICTURES = 80;
-    const PAINTED_MARKER = 23;
-    const PAINTED_PENCIL = 40;
-    const PAINTED_PAINTS = ALL_PICTURES - (PAINTED_MARKER + PAINTED_PENCIL);
+    $age = 1;
 
-    echo PAINTED_PAINTS;
-
+    if($age >= 1 && $age <= 17){
+        echo 'Вам еще рано работать';
+    } elseif($age >= 18 && $age <= 65){
+        echo 'Вам еще работать и работать';
+    } elseif($age > 65){
+        echo 'Вам пора на пенсию';
+    } else {
+        echo 'Неизвестный возраст';
+    }
 ?>
