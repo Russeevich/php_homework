f431aa7ebfa16f339e07c9100dfe6586c5f4f9c2
Russeevich <kokanazar@yandex.ru>
2021-08-18 13:12:45 +0300

    update

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?
-    $age = 1;
+    $day = 0;
 
-    if($age >= 1 && $age <= 17){
-        echo 'Вам еще рано работать';
-    } elseif($age >= 18 && $age <= 65){
-        echo 'Вам еще работать и работать';
-    } elseif($age > 65){
-        echo 'Вам пора на пенсию';
-    } else {
-        echo 'Неизвестный возраст';
+    switch($day){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            echo 'Это рабочий день';
+            break;
+        case 6:
+        case 7:
+            echo 'Это выходной день';
+            break;
+        default:
+            echo 'Неизвестный день';
+            break;
     }
 ?>
