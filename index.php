c08fe2d49fc462e527f6cf6a9003f25e959ca1fb
Russeevich <kokanazar@yandex.ru>
2021-08-18 12:35:27 +0300

    update

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php
-    
+<?
+    const TEST_CONSTANTA = 'asd';
 
+    $user_name = 'Igor';
+
+    function showSomething(){
+        return 'SOMETHING';
+    }
+
+    if(1 == 1){
+        echo 'hi';
+    }
 
 ?>
