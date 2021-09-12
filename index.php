356605e9c9e5c062a102894b6457cf53063baeff
Russeevich <kokanazar@yandex.ru>
2021-09-12 20:10:09 +0300

    future/6.3 first update

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-    
 
+use Intervention\Image\ImageManager;
 
-?>
+require_once "vendor/autoload.php";
+
+$manager = new ImageManager(["driver" => "imagick"]);
+
+$image = $manager->make(__DIR__ . DIRECTORY_SEPARATOR . 'photo.png')
+        ->crop(200, 0)
+        ->insert("https://internetsotrudnik.ru/wp-content/uploads/2017/05/unnamed-2-200x200.jpg", 'center')
+        ->save(__DIR__ . DIRECTORY_SEPARATOR . 'newPhoto.png', 100);
