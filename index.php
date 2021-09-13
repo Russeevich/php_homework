b718f26d978bb46d78f27a7110adf4279a59d2d2
Russeevich <kokanazar@yandex.ru>
2021-09-13 11:25:44 +0300

    future/6.3 update

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
 
+use Intervention\Image\AbstractFont;
+use Intervention\Image\Constraint;
 use Intervention\Image\ImageManager;
 
 require_once "vendor/autoload.php";
 
 $manager = new ImageManager(["driver" => "imagick"]);
 
-$image = $manager->make(__DIR__ . DIRECTORY_SEPARATOR . 'photo.png')
-        ->crop(200, 0)
-        ->insert("https://internetsotrudnik.ru/wp-content/uploads/2017/05/unnamed-2-200x200.jpg", 'center')
-        ->save(__DIR__ . DIRECTORY_SEPARATOR . 'newPhoto.png', 100);
+$img = $manager->make(__DIR__ . DIRECTORY_SEPARATOR . 'photo.png');
+
+echo $img->resize(200, null, function (Constraint $option){
+            $option->aspectRatio();
+        })
+        ->text('Loftschool', $img->getWidth() / 2, $img->getHeight() / 2, function (AbstractFont $font){
+            $font->size(60);
+            $font->color([0, 0, 0, 0.5]);
+            $font->file('Sirius Cursiv.ttf');
+            $font->valign('center');
+            $font->align('center');
+        })
+        ->response('png', 100);
